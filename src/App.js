55263b3418fe90eac37a5bcb58d7038d23e31516
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-11-15 04:51:32 +0200

    add base styles for app, searchbar, imageGallery and logic of API and searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import './App.css';
+import { Component } from 'react';
 
-export default function App() {
-  return 'Hello world!';
+import Searchbar from './components/Searchbar/Searchbar';
+
+class App extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleFormSubmit = inputValue => {
+    this.setState({ inputValue });
+  };
+
+  render() {
+    return (
+      <>
+        Hello world!
+        <Searchbar forSubmit={this.handleFormSubmit} />
+      </>
+    );
+  }
 }
+export default App;
